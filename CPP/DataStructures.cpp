2e226595b50a784e71892ec0bc83822c74f98c84
Anh Nguyen <44261379+BatmanofZuhandArrgh@users.noreply.github.com>
2020-05-15 19:02:58 +0700

    Create DataStructures.cpp

---
+++ CPP/DataStructures.cpp
@@ -0,0 +1,85 @@
+//
+//  main.cpp
+//  DataStructures
+//
+//  Created by Macbook on 5/14/20.
+//  Copyright © 2020 Anh Nguyen. All rights reserved.
+//
+
+#include <iostream>
+//#include "DataStructure.Array.hpp"
+#include "DataStructure.SinglyLinkedList.hpp"
+
+int main() {
+//    //I. Testing Dynamic Arrays:
+//    DynamicArray<int> Array;
+//    Array.push(1);
+//    Array.push(2);
+//    Array.push(3);
+//    Array.push(4);
+//    Array.print();
+//    std::cout << "The size of the array: " << Array.getSize() << std::endl;
+//
+//    //std::cout << "The last element being pop is: " << Array.pop() << std::endl;
+//    //std::cout << "The size of the array: " << Array.getSize() << std::endl;
+//
+//    int index = 2;
+//
+//    std::cout << "The element being removed at " << index << " is: " << Array.remove(index) << std::endl;
+//    std::cout << "The size of the array: " << Array.getSize() << std::endl;
+//    Array.print();
+//
+//    int newvalue = 16;
+//    std::cout << "Replacing the element at index " << index << " with new value " << newvalue << "." << std::endl;
+//    Array.insert(newvalue, index);
+//    Array.print();
+//
+////    DynamicArray<int> Array1 = DynamicArray<int>(Array); //Testing copy constructor
+////    Array1.print();
+//
+////    Array.reSize(15); //Testing Resize
+////    Array.print();
+////    std::cout << "The size of the array: " << Array.getSize() << std::endl;
+//    Array.push(1);
+//    Array.push(2);
+//    Array.push(3);
+//    Array.push(4);
+//    Array.push(1);
+//    Array.push(2);
+//    Array.push(3);
+//    Array.push(4);
+//    Array.print();
+//
+//    DynamicArray<int> MatchingPosition = Array.getPosition(1);
+//    MatchingPosition.print();
+//
+//    DynamicArray<int> Array1 = DynamicArray<int>(MatchingPosition); //Testing overloaded operator
+//    Array1.print();
+//    Array1.push(0);
+//    bool a = (Array1!=MatchingPosition);
+//    std::cout <<a<<std::endl;
+    
+    
+//    _________________________________________________
+    //II. Testing Linked List
+    SinglyLinkedList<int> LinkedList;
+    LinkedList.display();
+    LinkedList.createnode(1);
+    LinkedList.createnode(2);
+    LinkedList.createnode(3);
+    LinkedList.createnode(4);
+    LinkedList.createnode(5);
+    LinkedList.createnode(1);
+    LinkedList.display();
+    std::cout << "\n";
+    std::cout << LinkedList.size()<< std::endl;//Testing the number of elements in list
+    
+    LinkedList.insert_position(20, 2); //Testing insert at the beginning
+    LinkedList.display();
+    std::cout << "\n";
+    std::cout << LinkedList.size()<< std::endl;
+
+
+    
+    return 0;
+}
